e62d9dc57e418da2f9aa4211d900c156f86a8069
juzixiong <1480778564@qq.com>
2026-05-19 17:33:22 +0800

    :bug: 添加商户缓存业务代码开发

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.SystemConstants;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 
 /**
  * <p>
@@ -16,5 +25,41 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 根据id查询商铺信息
+     * @param id 商铺id
+     * @return 商铺详情数据
+     */
+    @Override
+    public Result queryById(Long id) {
+        //1 从redis查询商铺缓存
+        String key = CACHE_SHOP_KEY + id;
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        //2 判断缓存是否命中
+        if (StrUtil.isNotBlank(shopJson)) {
+            //3 存在 转成shop对象返回
+            Shop shop = JSONUtil.toBean(shopJson , Shop.class);
+            return Result.ok(shop);
+        }
+
+        //4 不存在则从数据库查询
+        Shop shop = getById(id);
+        //5 商铺不存在则返回 404
+        if (shop == null) {
+            return Result.fail("商铺不存在");
+        }
+        //6 存在则 写入redis 返回商铺信息
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop));
+        return Result.ok(shop);
+    }
+
+
+
+
+
 
 }
