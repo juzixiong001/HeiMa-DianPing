18d42a9783de635c543d7ca1076552fd9f4b368a
juzixiong <1480778564@qq.com>
2026-05-29 14:29:48 +0800

    :sparkles: 附近商户 业务代码开发

--- src/main/java/com/hmdp/service/IShopService.java
+++ src/main/java/com/hmdp/service/IShopService.java
@@ -28,4 +28,6 @@ public interface IShopService extends IService<Shop> {
      * @return 无
      */
     Result update(Shop shop);
+
+    Result queryShopByType(Integer typeId, Integer current, Double x, Double y);
 }
